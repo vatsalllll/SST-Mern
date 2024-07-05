a2ffedf332fdfbff6d78c711563f90cc99219ee9
mrinal1224 <iammrinalkc@gmail.com>
2024-07-05 10:53:47 +0530

    push

--- project/index.js
+++ project/index.js
@@ -12,6 +12,35 @@ mongoose
     console.log("Failed" , err);
   });
 
+
+  // ProductSchema
+
+  const productSchema = new mongoose.Schema({
+    product_name : {
+        type : String,
+        required : true
+    },
+    product_price : {
+        type : String,
+        required : true
+    },
+    isInStock : {
+        type : Boolean,
+        required : true
+    },
+    category : {
+     type : String,
+     required : true
+
+    }
+
+
+
+  })
+
+
+
+
 const app = express();
 
 app.listen(8086, () => {
